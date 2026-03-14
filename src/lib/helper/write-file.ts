27bdaa4831cefaab2b9fa4621d20114fedf9bdf5
Riyan Fanani <ogickarecka1108@gmail.com>
2026-03-15 05:48:06 +0700

    update: write video file function logic and add delete file function

--- src/lib/helper/write-file.ts
+++ src/lib/helper/write-file.ts
@@ -1,24 +1,45 @@
 import { createId } from '@paralleldrive/cuid2'
+import fs from 'fs'
 import path from 'path'
-import sharp from 'sharp'
 
 export async function writeVideoFile(file: File) {
 	try {
 		const dirname = process.cwd()
-		const imageDir = path.join(dirname, 'upload', 'images')
+		const videoDir = path.join(dirname, 'upload', 'video')
 		const formattedName = file.name
 			.split('.')[0]
 			.replace(/[^a-zA-Z0-9]/g, '')
 			.toLowerCase()
 			.slice(0, 50)
-		const fileName = `${createId()}-${formattedName}.webp`
-		const filePath = path.join(imageDir, fileName)
+		const fileName = `${createId()}-${formattedName}.${file.type.split('/')[1]}`
+		const filePath = path.join(videoDir, fileName)
 		const buffer = Buffer.from(await file.arrayBuffer())
 
-		await sharp(buffer).webp({ quality: 80 }).toFile(filePath)
+		fs.writeFileSync(filePath, new Uint8Array(buffer))
 
 		return fileName
 	} catch {
 		return null
+	}
+}
+
+export async function deleteFile(relativePath: string | null | undefined): Promise<boolean> {
+	if (!relativePath) return true
+
+	try {
+		const dirname = process.cwd()
+
+		const absolutePath = path.join(
+			dirname,
+			relativePath.startsWith('/') ? relativePath.slice(1) : relativePath
+		)
+
+		if (fs.existsSync(absolutePath)) {
+			fs.unlinkSync(absolutePath)
+		}
+
+		return true
+	} catch {
+		return false
 	}
 }
